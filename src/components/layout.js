b60cb3716ee4f9bb2ccf9d745a17c0ace52d653f
Seth Hall <seth@ushahidi.com>
2020-03-20 17:31:45 -0400

    layout, nav, mobilenav, page set up

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import { HelmetDatoCms } from "gatsby-source-datocms"
@@ -10,8 +10,17 @@ import { MobileNav, NavLeft, NavRight, FooterNav } from "./nav"
 
 // Styles
 import { Wrapper, Header, Footer } from "../styles/Layout"
-
+  
 const Layout = ({ location, children }) => {
+
+  useEffect(() => {
+    const location = location.pathname;
+  });
+  
+  const pathName = () => {
+    return location
+  }
+
   return (
     <Wrapper>
       <HelmetDatoCms>
@@ -20,14 +29,14 @@ const Layout = ({ location, children }) => {
           crossorigin="anonymous"
         ></script>
       </HelmetDatoCms>
-      <MobileNav location={location.pathname} />
+      <MobileNav location={pathName()} />
       <div className="app">
         <Header>
-          <NavLeft location={location.pathname} />
+          <NavLeft location={pathName()} />
           <h3 className="title">
             <Link to={`/`}>Together UBI</Link>
           </h3>
-          <NavRight location={location.pathname} />
+          <NavRight location={pathName()} />
         </Header>
         <main>{children}</main>
       </div>

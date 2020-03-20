a396827c2c39dd27b316048529ae633b5741ddb2
Seth Hall <seth@ushahidi.com>
2020-03-20 13:43:28 -0400

    intial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,41 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Together UBI`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    // author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-image`,
+    `gatsby-plugin-styled-components`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-source-datocms`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        apiToken: `34ecf76bb301fb616e6a8cab75d254`,
+        preview: false,
+        disableLiveReload: false,
       },
     },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Together UBI`,
+        short_name: `Together UBI`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,

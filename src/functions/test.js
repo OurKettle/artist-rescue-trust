bf48e585f394dbf882e88cd3a6916f01c76da729
Seth Hall <seth@ushahidi.com>
2020-04-15 12:36:56 -0400

    working on test function

--- src/functions/test.js
+++ src/functions/test.js
@@ -1,16 +1,23 @@
 exports.handler = async (event, context) => {
-  let formData = JSON.parse(event.body)
-  console.log(formData)
+  // let formData = JSON.parse(event.body)
+  // console.log(formData)
 
-  if (errors) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify(errors),
-    }
-  }
-  
+  // if (errors) {
+  //   return {
+  //     statusCode: 500,
+  //     body: JSON.stringify(errors),
+  //   }
+  // }
+
+  // return {
+  //   statusCode: 200,
+  //   body: formData,
+  // }
+
+  console.log("this is a console log")
+  let log = "this is the body"
   return {
     statusCode: 200,
-    body: formData,
+    body: log,
   }
 }

e087f7716fbfb98bba26772178a1257293e89c57
Seth Hall <seth@ushahidi.com>
2020-04-15 14:59:14 -0400

    test function

--- src/functions/test.js
+++ src/functions/test.js
@@ -2,11 +2,13 @@ export async function handler(event, context) {
   const fetch = require("node-fetch")
 
   fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then(response => response.json())
-    .then(json => console.log(json))
+    .then(resp => resp.json())
+    .then(function(data) {
+      const data = data.results
+    })
 
   return {
     statusCode: 200,
-    body: "this is the body",
+    body: data.results.title,
   }
 }

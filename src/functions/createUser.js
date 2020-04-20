5c623d7767f527e40847450f104711301a71d3cd
Seth Hall <seth@ushahidi.com>
2020-04-20 13:57:03 -0400

    Update createUser.js

--- src/functions/createUser.js
+++ src/functions/createUser.js
@@ -1,16 +1,32 @@
-exports.handler = async (event, context) => {
-  const user = JSON.parse(event.body)
-  console.log(user)
+const faunadb = require("faunadb")
 
-  // if (errors) {
-  //   return {
-  //     statusCode: 500,
-  //     body: JSON.stringify(errors),
-  //   }
-  // }
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.fnADp5Xx5gACEyuIYWZera0bRNxvWshHKhXyyjx7,
+})
 
-  return {
-    statusCode: 200,
-    body: user,
+exports.handler = (event, context, callback) => {
+  const data = JSON.parse(event.body)
+  console.log("Function `createUser` invoked", data)
+  const user = {
+    data: data,
   }
+
+  /* construct the fauna query */
+  return client
+    .query(q.Create(q.Ref("classes/user"), user))
+    .then(response => {
+      console.log("success", response)
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(response),
+      })
+    })
+    .catch(error => {
+      console.log("error", error)
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error),
+      })
+    })
 }

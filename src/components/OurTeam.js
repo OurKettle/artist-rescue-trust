4dc0751258b541cc859f5c7633d8aae47471da3c
Seth Hall <seth@ushahidi.com>
2020-04-21 10:27:20 -0400

    refactors Our Team

--- src/components/OurTeam.js
+++ src/components/OurTeam.js
@@ -13,20 +13,13 @@ const OurTeam = () => {
     <StaticQuery
       query={ourTeamQuery}
       render={data => {
-        // const kettleLogo = data.kettleLogo
-        // const ocupopLogo = data.ocupopLogo
-        // const yolLogo = data.yolLogo
-
         const team = data.ourTeam
-        // const contentBlock1 = data.ourTeam.contentBlocks[0]
-        // const contentBlock2 = data.ourTeam.contentBlocks[1]
-        // const contentBlock3 = data.ourTeam.contentBlocks[2]
-        // const contentBlock4 = data.ourTeam.contentBlocks[3]
+        const teams = data.ourTeam.teamContentBlocks
 
         return (
           <StyledTeam>
             <StyledGrid className="main-content">
-              <div className="box a">
+              <div className="box">
                 <h1 className="heading">{team.heading}</h1>
                 <div
                   dangerouslySetInnerHTML={{
@@ -35,45 +28,22 @@ const OurTeam = () => {
                 />
                 <hr></hr>
               </div>
-              {/* <div className="box b">
-                <Img
-                  className="team-logo"
-                  fluid={kettleLogo.fluid}
-                  duration={1000}
-                  alt="Kettle Logo"
-                />
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: contentBlock2.bodyNode.childMarkdownRemark.html,
-                  }}
-                />
-              </div>
-              <div className="box c">
-                <Img
-                  className="team-logo"
-                  fluid={ocupopLogo.fluid}
-                  duration={1000}
-                  alt="Ocupop Logo"
-                />
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: contentBlock3.bodyNode.childMarkdownRemark.html,
-                  }}
-                />
-              </div>
-              <div className="box d">
-                <Img
-                  className="team-logo"
-                  fluid={yolLogo.fluid}
-                  duration={1000}
-                  alt="Yol Logo"
-                />
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: contentBlock4.bodyNode.childMarkdownRemark.html,
-                  }}
-                />
-              </div> */}
+
+              {teams.map(team => (
+                <div className="box" key={team.id}>
+                  <Img
+                    className="team-logo"
+                    fluid={team.logo.fluid}
+                    duration={1000}
+                    alt="Kettle Logo"
+                  />
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: team.bodyNode.childMarkdownRemark.html,
+                    }}
+                  />
+                </div>
+              ))}
             </StyledGrid>
           </StyledTeam>
         )
@@ -94,7 +64,11 @@ export const ourTeamQuery = graphql`
       teamContentBlocks {
         ... on DatoCmsTeamContentBlock {
           id
-          # logo
+          logo {
+            fluid(maxWidth: 400) {
+              ...GatsbyDatoCmsFluid
+            }
+          }
           bodyNode {
             childMarkdownRemark {
               html
@@ -103,21 +77,6 @@ export const ourTeamQuery = graphql`
         }
       }
     }
-    # kettleLogo: datoCmsAsset(filename: { eq: "kettle-logo.png" }) {
-    #   fluid(maxWidth: 400, maxHeight: 181) {
-    #     ...GatsbyDatoCmsFluid
-    #   }
-    # }
-    # ocupopLogo: datoCmsAsset(filename: { eq: "ocupop-logo.png" }) {
-    #   fluid(maxWidth: 400, maxHeight: 181) {
-    #     ...GatsbyDatoCmsFluid
-    #   }
-    # }
-    # yolLogo: datoCmsAsset(filename: { eq: "yol-logo.png" }) {
-    #   fluid(maxWidth: 400, maxHeight: 181) {
-    #     ...GatsbyDatoCmsFluid
-    #   }
-    # }
   }
 `
 

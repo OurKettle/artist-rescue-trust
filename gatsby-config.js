393aa3fd386975e2722ac15057b77aeed08d7e38
Seth Hall <seth@ushahidi.com>
2020-04-24 15:23:34 -0400

    updates favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
         // background_color: `#663399`,
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/white-logomark.png`, // This path is relative to the root of the site.
+        icon: `src/images/logomark.png`, // This path is relative to the root of the site.
       },
     },
     {
